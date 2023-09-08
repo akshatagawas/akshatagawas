4aff3ce2dd1fae2b70a3504033bcd3462c031200
agawas <agawas@iu.edu>
2023-09-08 17:06:05 -0400

    Initial

---
+++ main.js
@@ -0,0 +1,36 @@
+var typed = new Typed(".text", {
+    strings:["Software Engineer","Full-Stack Developer","Frontend Developer","Trekker"],
+    typeSpeed:50,
+    backSpeed:25,
+    backDelay:1000,
+    loop:true
+})
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
